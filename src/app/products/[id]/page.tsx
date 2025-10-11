faa01e18dd66ec4f6a4b830c19b6407d3ccce0b5
WebApp07 <aminezouguig@proton.me>
2025-10-11 03:45:00 -0700

    implement seo nextjs static title desc for product page & home page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -20,6 +20,19 @@ const product: ProductType = {
   },
 };
 
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { id: string };
+}) => {
+  // TODO:get the product from db
+  // TEMPORERY
+  return {
+    title: product.name,
+    describe: product.description,
+  };
+};
+
 const ProductPage = async ({
   params,
   searchParams,
